759e604f1d9a23bcb9f91b8b02e5541b476f8dc8
wyfrn <daniel.priebe@syn-com.de>
2011-06-11 17:42:35 +0200

    fixed some problems with readValue

--- src/Cashlog/InOut.hs
+++ src/Cashlog/InOut.hs
@@ -4,44 +4,23 @@ import Control.Exception
 import Data.Maybe
 import Types
 
--- | The 'readValue'' function promts the user for a value of
+-- | The 'readValue' function promts the user for a value of
 -- type 'a' until a successfully read was performed.
-readValue' :: Read a => 
-              String ->
-              IO a
-readValue' msg = do
-	putStr $ msg ++ ": "
-	tryRead <- try readLn :: Read a => IO (Either SomeException a)
-	case tryRead of
-		Left _ -> readValue' msg
-		Right val -> return val
 
--- | The 'readValue''' function is similar to 'readValue'' 
--- except that it returns a default value if input was empty.
-readValue'' :: Read a => 
-               String ->
-               a ->
-               IO a
-readValue'' msg def = do
-    putStr $ msg ++ ": "
-    raw <- getLine
-    case raw of
-        [] -> return def
-        _ -> do tryRead <- try $ readIO raw :: Read a => IO ( Either SomeException a )
-                case tryRead of
-                    Left e -> do print e 
-                                 readValue'' msg def
-                    Right val -> return val
-
--- | The 'readValue' function decides wether to use 'readValue'' or 'readValue'''
--- depending on if a default value was given or not.
 readValue :: Read a =>
              String ->
              Maybe a ->
              IO a
-readValue msg (Just def) = readValue'' msg def
-readValue msg _ = readValue' msg
-
+readValue msg def = do
+    putStr $ msg ++ ": "
+    raw <- getLine
+    if isJust def && length raw == 0
+        then return $ fromJust def
+        else do tryRead <- try $ readIO raw :: Read a => IO ( Either SomeException a )
+                case tryRead of
+                  Left _ -> readValue msg def
+                  Right val -> return val
+                
 -- | The 'maybeAccessor' function applies the accessor 'f' 
 -- if a valid 'a' is given.
 maybeAccessor :: Read b =>
