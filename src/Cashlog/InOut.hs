d1c15c11d40492435dd03fe756e89611bfe5969f
wyfrn <daniel.priebe@syn-com.de>
2011-06-12 00:47:23 +0200

    - maybe we've got the final version of readValue :-) - some more read* functions

--- src/Cashlog/InOut.hs
+++ src/Cashlog/InOut.hs
@@ -6,36 +6,44 @@ import Types
 
 -- | The 'readValue' function promts the user for a value of
 -- type 'a' until a successfully read was performed.
-
-readValue :: Read a =>
-             String ->
-             Maybe a ->
-             IO a
+readValue :: Read a => String -> Maybe a -> IO a
 readValue msg def = do
-    putStr $ msg ++ ": "
-    raw <- getLine
-    if isJust def && length raw == 0
-        then return $ fromJust def
-        else do tryRead <- try $ readIO raw :: Read a => IO ( Either SomeException a )
-                case tryRead of
-                  Left _ -> readValue msg def
-                  Right val -> return val
-                
--- | The 'maybeAccessor' function applies the accessor 'f' 
--- if a valid 'a' is given.
-maybeAccessor :: Read b =>
-                 ( a -> b ) ->
-                 Maybe a ->
-                 Maybe b
-maybeAccessor f (Just a) = Just $ f a
-maybeAccessor _ _ = Nothing
+    raw <- readString msg Nothing
+    case (raw,def) of
+      ([], Just d) -> return d
+      _            -> do tryRead <- try $ readIO raw :: Read a => IO ( Either SomeException a )
+                         case tryRead of
+                           Left _ -> readValue msg def
+                           Right val -> return val
 
+-- | The 'readString' function is a specialization of the 'readValue' function.
+readString :: String -> Maybe String -> IO String
+readString msg def = do
+    putStr $ msg ++ ": "
+    str <- getLine
+    return $ case (str,def) of
+      ([], Just d) -> d
+      _            -> str
 
 readArticle :: Maybe Article ->
                IO Article
 readArticle a = do
-    n <- readValue "Bezeichnung" ( maybeAccessor articleName a )
-    p <- readValue "Preis" ( maybeAccessor  articlePrice a )
-    c <- readValue "Kategorie" ( maybeAccessor articleCategoryId a )
+    n <- readString "Bezeichnung" $ fmap articleName a
+    p <- readValue  "Preis"       $ fmap articlePrice a
+    c <- readValue " Kategorie"   $ fmap articleCategoryId a
     return $ Article  (-1) n p c
+
+readCategory :: Maybe Category ->
+                IO Category
+readCategory c = do
+    p <- readValue  "Oberkategorie" $ fmap categoryParent c
+    n <- readString "Name"          $ fmap categoryName c
+    return $ Category (-1) p n
+
+readShop :: Maybe Shop ->
+            IO Shop
+readShop s = do
+    n <- readString "Name"  $ fmap shopName s
+    c <- readString "Stadt" $ fmap shopCity s
+    return $ Shop (-1) n c
 
